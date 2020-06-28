f973418755e34a734afce2ab2c93ac13b604332e
bvlion <iwai.satoshi@zaim.co.jp>
2020-06-28 23:10:48 +0900

    SweetAlert のバージョン変更とインデント修正

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,45 +1,45 @@
 const login = () => {
-    const passphrase = document.querySelector('#passphrase').value
-    if (!passphrase) {
-      swal('合言葉を入力してください。', '', 'error')
-      return
-    }
-    document.querySelector('.loading-overlay').style.display = 'block'
-    document.querySelector('.loading-message').style.display = 'block'
-    document.querySelector('.loader').style.display = 'block'
-    fetch('/login', {
-      credentials: 'same-origin',
-      method: 'POST',
-      body: JSON.stringify({passphrase: passphrase}),
-      headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}
-    }).then((response) => {
-      return response.json()
-    }).then((json) => {
-      if (json.error) {
-        document.querySelector('.loading-overlay').style.display = 'none'
-        document.querySelector('.loading-message').style.display = 'none'
-        document.querySelector('.loader').style.display = 'none'
-        swal(json.error, '', 'error')
-      } else {
-        window.location = '/'
-      }
-    })
+  const passphrase = document.querySelector('#passphrase').value
+  if (!passphrase) {
+    Swal.fire('合言葉を入力してください。', '', 'error')
+    return
   }
-  
-  const enter = () => {
-    if (window.event.keyCode === 13) {
-      login()
-    }
-  }
-  
-  let view = false
-  const showChange = () => {
-    const passphrase = document.querySelector('#passphrase')
-    if (view) {
-      passphrase.setAttribute("type", "password")
-      view = false
+  document.querySelector('.loading-overlay').style.display = 'block'
+  document.querySelector('.loading-message').style.display = 'block'
+  document.querySelector('.loader').style.display = 'block'
+  fetch('/login', {
+    credentials: 'same-origin',
+    method: 'POST',
+    body: JSON.stringify({passphrase: passphrase}),
+    headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}
+  }).then((response) => {
+    return response.json()
+  }).then((json) => {
+    if (json.error) {
+      document.querySelector('.loading-overlay').style.display = 'none'
+      document.querySelector('.loading-message').style.display = 'none'
+      document.querySelector('.loader').style.display = 'none'
+      Swal.fire(json.error, '', 'error')
     } else {
-      passphrase.setAttribute("type", "text")
-      view = true
+      window.location = '/'
     }
+  })
+}
+
+const enter = () => {
+  if (window.event.keyCode === 13) {
+    login()
   }
+}
+
+let view = false
+const showChange = () => {
+  const passphrase = document.querySelector('#passphrase')
+  if (view) {
+    passphrase.setAttribute("type", "password")
+    view = false
+  } else {
+    passphrase.setAttribute("type", "text")
+    view = true
+  }
+}
